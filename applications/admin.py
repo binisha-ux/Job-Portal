a959862eebc87e7044c93e539804e8cf7954879f
binisha-ux <beenesha00@gmail.com>
2026-09-10 15:41:38 +0545

    Register Application model in Django admin

--- applications/admin.py
+++ applications/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Application
 
 # Register your models here.
+@admin.register(models.admin)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ("job", "candidate", "status", "applied_at")
+    list_filter = ("status", "applied_at")
+    search_fields = ("job__title", "candidate__user__username", "candidate__user__email")
+
+    ordering = ("-applied_at",)
