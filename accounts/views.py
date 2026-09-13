7d5985b59f974cbdb690427c03410c4b28fc9484
binisha-ux <beenesha00@gmail.com>
2026-09-13 14:06:57 +0545

    fixed some architecture of employer_signin and signup

--- accounts/views.py
+++ accounts/views.py
@@ -51,5 +51,50 @@ def sign_in(request):
 
 
 
+
+
 def login_view(request):
     return render(request, "login.html")
+
+
+def employer_signup(request):
+    if request.method == "POST":
+        data = request.POST
+
+        email = data.get('email')
+        password = data.get('password')
+
+        if User.objects.filter(username=email).exists():
+            messages.info(request, "Email already existed")
+            return redirect("employer_signup")
+
+        user = User.objects.create_user(username=email, email=email, password=password)
+        login(request, user , backend='django.contrib.auth.backends.ModelBackend')
+        return redirect('jobportal')
+
+    return render(request, "employer_signup.html")
+
+def employer_home(request):
+    return render(request, "employer.html")
+
+
+def employer_signin(request):
+    if request.method == "POST":
+        data = request.POST
+        
+        email = data.get('email')
+        password = data.get('password')
+
+        user = authenticate(request, username=email, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.info(request, "Logged in succesfully. ")
+            return redirect('jobportal')
+        
+        else:
+            messages.info(request, "invalid credentials. ")
+            return redirect("employer_signin")
+
+        
+    return render(request, "employer_signin.html")
