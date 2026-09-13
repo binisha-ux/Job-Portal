ddb0baf8a1f34f0c9d4a6487fb19a16747bf4c29
binisha-ux <beenesha00@gmail.com>
2026-09-13 12:44:51 +0545

    Fix authenticate parameters in employer_signin vie

--- jobs/views.py
+++ jobs/views.py
@@ -24,3 +24,46 @@ def employer_signup(request):
 
 def employer_home(request):
     return render(request, "employer.html")
+
+
+def employer_signin(request):
+    if request.method == "POST":
+        data = request.POST
+        
+        email = data.get('email')
+        password = data.get('password')
+
+        user = authenticate(request, username=email, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.info(request, "Logged in succesfully. ")
+            return redirect('jobportal')
+        
+        else:
+            messages.info(request, "invalid credentials. ")
+            return redirect("employer_signin")
+
+        
+    return render(request, "employer_signin.html")
+
+
+def employer_signin(request):
+    if request.method == "POST":
+        data = request.POST
+
+        email = data.get("email")
+        password = data.get("password")
+
+        user = authenticate(request, username=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "logged in successfully. ")
+            return redirect('jobportal')
+
+        else:
+            messages.error(request, "invalid credentials. ")
+            return redirect('employer_signin')
+
+    return render(request, "employer_signin.html")
