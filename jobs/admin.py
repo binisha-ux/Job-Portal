b80554d37a265e98820413f9ed6f7599d982b415
binisha-ux <beenesha00@gmail.com>
2026-09-13 14:02:55 +0545

    fixed errors in regsitered jobs/models.py in admin.py

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.models import Job
+from .models import Job
 # Register your models here.
-@admin.site.register
+@admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
-    list_display = ('employer', 'title', 'job_type', salary)
+    list_display = ('employer', 'title', 'job_type', 'salary')
     list_filter = ('job_type', 'description', 'created_at')
-    search_fields = ('title', description 'location', 'job_type', 'salary')
+    search_fields = ('title', 'description', 'location', 'job_type', 'salary')
