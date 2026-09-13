e0aef67477f7d23298c0d0d95e78ad0e852a55e9
binisha-ux <beenesha00@gmail.com>
2026-09-13 12:28:15 +0545

    finished work on employer_signup.html

--- jobs/views.py
+++ jobs/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages 
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+
 
 # Create your views here.
 def employer_signup(request):
+    if request.method == "POST":
+        data = request.POST
+
+        email = data.get('email')
+        password = data.get('password')
+
+        if User.objects.filter(username=email).exists():
+            messages.info(request, "Email already existed")
+            return redirect("employer_signup")
+
+        user = User.objects.create_user(username=email, email=email, password=password)
+        login(request, user , backend='django.contrib.auth.backends.ModelBackend')
+        return redirect('jobportal')
+
     return render(request, "employer_signup.html")
 
 def employer_home(request):
