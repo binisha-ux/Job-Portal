2853198c316edc1c6efdf2b2ce9e562c3b94b6ad
binisha-ux <beenesha00@gmail.com>
2026-09-14 15:41:58 +0545

    Fix job update logic and context rendering in update_job view

--- job_portal/urls.py
+++ job_portal/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from accounts.views import home , sign_in,  sign_up
 from accounts.views import employer_signup, employer_home, employer_signin
-from jobs.views import create_job, delete_job
+from jobs.views import create_job, delete_job, update_job
 
 
 urlpatterns = [
@@ -32,6 +32,8 @@ urlpatterns = [
     path('employer_signin/', employer_signin, name="employer_signin"),
     path('createjob/', create_job, name="create_job"),
     path('deletejob/<int:job_id>/', delete_job, name="delete_job"),
+    path('updatejob/<int:job_id>/', update_job, name="update_job"),
+
 
 
     
--- jobs/views.py
+++ jobs/views.py
@@ -49,3 +49,25 @@ def delete_job(request, job_id):
         return redirect("create_job")
 
     return redirect("create_job")
+
+@login_required
+def update_job(request, job_id):
+    job = get_object_or_404(Job, id=job_id, employer=request.user)
+
+    if request.method == "POST":
+        data = request.POST
+
+        job.title = data.get('job_title', job.title)
+        job.location = data.get('location', job.location)
+        job.salary = data.get('salary', job.salary)
+        job.job_type = data.get('job_type', job.job_type)
+        job.description = data.get('description', job.description)
+        job.company_name = data.get('company_name', job.company_name)
+
+        job.save()
+
+        messages.success(request, "Job created successfully.")
+        return redirect('create_job')
+
+
+    return render(request, "update_job.html", {'job':job})
