bae09e1c0f6f160eb393866615be45051e327b40
binisha-ux <beenesha00@gmail.com>
2026-09-16 14:58:40 +0545

    implement job application flow and alert messages

--- applications/views.py
+++ applications/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from jobs.models import Job
 from .models import Application
+from django.contrib import messages 
 
 # Create your views here.
 def apply_for_job(request, job_id):
@@ -10,7 +11,37 @@ def apply_for_job(request, job_id):
     if request.method == "POST":
         data = request.POST
 
-        
+        full_name = data.get('full_name')
+        email = data.get('email')
+        phone = data.get('phone')
+        portfolio_url = data.get('portfolio_url')
+        cover_letter = data.get('cover_letter')
+        resume = request.FILES.get('resume')
+
+        if Application.objects.filter(job=job, candidate=request.user).exists():
+            messages.warning(request, "You have already applied for this job.")
+            return redirect("search_jobs")
+
+
+        Application.objects.create(
+            job = job,
+            candidate = request.user,
+            full_name = full_name, 
+            email = email, 
+            phone = phone, 
+            portfolio_url = portfolio_url, 
+            cover_letter = cover_letter,
+            resume = resume
+        )
+        return redirect("search_jobs")
+
+    return render(request, "my_applications.html", {'job':job})
+
+
+
+
+ 
+
     
 
 
